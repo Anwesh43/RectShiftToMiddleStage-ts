55137e74001776aecce5ffd862e93da9f3c6b043
Anwesh43 <anweshthecool0@gmail.com>
2018-08-22 10:45:06 +0530

    adding State class

--- RectShiftToMiddleStage.ts
+++ RectShiftToMiddleStage.ts
@@ -28,3 +28,26 @@ class RectShiftToMiddleStage {
         }
     }
 }
+
+class State {
+    scale : number = 0
+    prevScale : number = 0
+    dir : number = 0
+
+    update(cb) {
+        this.scale += this.dir * 0.1
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
