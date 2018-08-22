9276a8722273122ecf946f136e3414389eb3c7b8
Anwesh43 <anweshthecool0@gmail.com>
2018-08-22 10:42:13 +0530

    adding RectShiftToMiddleStage class

---
+++ RectShiftToMiddleStage.ts
@@ -0,0 +1,30 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
+class RectShiftToMiddleStage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    constructor() {
+        this.initCanvas()
+        this.render()
+        this.handleTap()
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
